66c1b8d5b6803324a4e077235e4e09a4c76b8ce7
e4h2n <eshanbajwa@gmail.com>
2024-09-29 12:01:45 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,32 @@
+local opt = vim.opt
+local g = vim.g
+
+require("config.lazy")
+require("lazy").setup({
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+})
+require('lualine').setup {
+  options = {
+    theme = 'tokyonight'
+  }
+}
+
+opt["tabstop"] = 4
+opt["shiftwidth"] = 4
+opt.relativenumber = true
+opt.number = true
+opt.cursorline = true
+
+g.loaded_ruby_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_python3_provider = 0
+
+-- disable automatic comment prefix when pressing 'enter' on a commented line
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove { "c", "r", "o" }
+  end,
+  desc = "Disable New Line Comment",
+})
+
+vim.cmd [[colorscheme tokyonight]]
